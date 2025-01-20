452e00a19c279ef32e1c26261201341ea7bab7d7
Hector Leon <hecosleco@gmail.com>
2025-01-19 19:35:01 -0500

    Agregando configuración de archivos estáticos

--- mercado_logistico/settings_prod.py
+++ mercado_logistico/settings_prod.py
@@ -6,18 +6,20 @@ ALLOWED_HOSTS = [
     '.onrender.com',
 ]
 
-# Configuración de archivos estáticos
+# Configuración de archivos estáticos con whitenoise
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Configuración de archivos media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Configuraciones de email para producción
+# Resto de tus configuraciones...
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -26,5 +28,4 @@ EMAIL_HOST_USER = 'hecosleco@gmail.com'
 EMAIL_HOST_PASSWORD = 'cbdy wtcp dvfx wlmj'
 DEFAULT_FROM_EMAIL = 'hecosleco@gmail.com'
 
-# Clave secreta de producción
 SECRET_KEY = 'django-insecure-h7k9#m2ny4s@px8z*5$_9#j3t$_q+v!z&k4wq!8e^5ph_5k2p&'
